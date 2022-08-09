b74dc436d8c30dfc3ee1d14f84ca474d603837c3
yuhongda <silverage.y@gmail.com>
2022-08-09 18:00:39 +0800

    🔧 opt: export props type

--- packages/echarts-readymade/src/index.ts
+++ packages/echarts-readymade/src/index.ts
@@ -1,4 +1,3 @@
-
 export * from '@echarts-readymade/core'
 import { Bar } from './bar'
 import { Line } from './line'
@@ -8,6 +7,24 @@ import { Stack } from './stack'
 import { Wordcloud } from './wordcloud'
 import { BarHorizontal } from './bar-horizontal'
 import { Table } from './table'
+import type { IWordcloudChartProps } from './wordcloud'
+import type { ITableChartProps } from './table'
+import type { IStackChartProps } from './stack'
+import type { IScatterChartProps } from './scatter'
+import type { IPieChartProps } from './pie'
+import type { ILineChartProps } from './line'
+import type { IBarChartProps } from './bar'
+import type { IBarHorizontalChartProps } from './bar-horizontal'
 
+export type {
+  IWordcloudChartProps,
+  ITableChartProps,
+  IStackChartProps,
+  IScatterChartProps,
+  IPieChartProps,
+  ILineChartProps,
+  IBarChartProps,
+  IBarHorizontalChartProps
+}
 export { Bar, Line, Pie, Scatter, Stack, Wordcloud, BarHorizontal, Table }
 export default { Bar, Line, Pie, Scatter, Stack, Wordcloud, BarHorizontal, Table }
--- packages/echarts-readymade/types/index.d.ts
+++ packages/echarts-readymade/types/index.d.ts
@@ -1,46 +1,20 @@
 import * as React from 'react';
 import React__default from 'react';
-import { TableChartProps } from '@echarts-readymade/table';
-import { ECharts } from 'echarts';
-import { BarHorizontalChartProps } from '@echarts-readymade/bar-horizontal';
-import { WordcloudChartProps } from '@echarts-readymade/wordcloud';
-import { StackChartProps } from '@echarts-readymade/stack';
-import { ScatterChartProps } from '@echarts-readymade/scatter';
-import { PieChartProps } from '@echarts-readymade/pie';
-import { LineChartProps } from '@echarts-readymade/line';
 import * as echarts_types_dist_echarts from 'echarts/types/dist/echarts';
-import { BarChartProps } from '@echarts-readymade/bar';
 export * from '@echarts-readymade/core';
+import { ECharts } from 'echarts';
+import { BarChartProps } from '@echarts-readymade/bar';
+import { LineChartProps } from '@echarts-readymade/line';
+import { PieChartProps } from '@echarts-readymade/pie';
+import { ScatterChartProps } from '@echarts-readymade/scatter';
+import { StackChartProps } from '@echarts-readymade/stack';
+import { WordcloudChartProps } from '@echarts-readymade/wordcloud';
+import { BarHorizontalChartProps } from '@echarts-readymade/bar-horizontal';
+import { TableChartProps } from '@echarts-readymade/table';
 
-interface ITableChartProps extends Omit<TableChartProps, 'context'> {
+interface IBarChartProps extends Omit<BarChartProps, 'context'> {
 }
-declare const Table: React__default.FC<ITableChartProps>;
-
-interface IBarHorizontalChartProps extends Omit<BarHorizontalChartProps, 'context'> {
-}
-declare const BarHorizontal: React__default.ForwardRefExoticComponent<IBarHorizontalChartProps & React__default.RefAttributes<{
-    getEchartsInstance: () => ECharts | undefined;
-}>>;
-
-interface IWordcloudChartProps extends Omit<WordcloudChartProps, 'context'> {
-}
-declare const Wordcloud: React__default.FC<IWordcloudChartProps>;
-
-interface IStackChartProps extends Omit<StackChartProps, 'context'> {
-}
-declare const Stack: React__default.ForwardRefExoticComponent<IStackChartProps & React__default.RefAttributes<{
-    getEchartsInstance: () => ECharts | undefined;
-}>>;
-
-interface IScatterChartProps extends Omit<ScatterChartProps, 'context'> {
-}
-declare const Scatter: React__default.ForwardRefExoticComponent<IScatterChartProps & React__default.RefAttributes<{
-    getEchartsInstance: () => ECharts | undefined;
-}>>;
-
-interface IPieChartProps extends Omit<PieChartProps, 'context'> {
-}
-declare const Pie: React__default.ForwardRefExoticComponent<IPieChartProps & React__default.RefAttributes<{
+declare const Bar: React__default.ForwardRefExoticComponent<IBarChartProps & React__default.RefAttributes<{
     getEchartsInstance: () => ECharts | undefined;
 }>>;
 
@@ -50,11 +24,37 @@ declare const Line: React__default.ForwardRefExoticComponent<ILineChartProps & R
     getEchartsInstance: () => ECharts | undefined;
 }>>;
 
-interface IBarChartProps extends Omit<BarChartProps, 'context'> {
+interface IPieChartProps extends Omit<PieChartProps, 'context'> {
 }
-declare const Bar: React__default.ForwardRefExoticComponent<IBarChartProps & React__default.RefAttributes<{
+declare const Pie: React__default.ForwardRefExoticComponent<IPieChartProps & React__default.RefAttributes<{
     getEchartsInstance: () => ECharts | undefined;
 }>>;
+
+interface IScatterChartProps extends Omit<ScatterChartProps, 'context'> {
+}
+declare const Scatter: React__default.ForwardRefExoticComponent<IScatterChartProps & React__default.RefAttributes<{
+    getEchartsInstance: () => ECharts | undefined;
+}>>;
+
+interface IStackChartProps extends Omit<StackChartProps, 'context'> {
+}
+declare const Stack: React__default.ForwardRefExoticComponent<IStackChartProps & React__default.RefAttributes<{
+    getEchartsInstance: () => ECharts | undefined;
+}>>;
+
+interface IWordcloudChartProps extends Omit<WordcloudChartProps, 'context'> {
+}
+declare const Wordcloud: React__default.FC<IWordcloudChartProps>;
+
+interface IBarHorizontalChartProps extends Omit<BarHorizontalChartProps, 'context'> {
+}
+declare const BarHorizontal: React__default.ForwardRefExoticComponent<IBarHorizontalChartProps & React__default.RefAttributes<{
+    getEchartsInstance: () => ECharts | undefined;
+}>>;
+
+interface ITableChartProps extends Omit<TableChartProps, 'context'> {
+}
+declare const Table: React__default.FC<ITableChartProps>;
 
 declare const _default: {
     Bar: React.ForwardRefExoticComponent<IBarChartProps & React.RefAttributes<{
@@ -79,4 +79,4 @@ declare const _default: {
     Table: React.FC<ITableChartProps>;
 };
 
-export { Bar, BarHorizontal, Line, Pie, Scatter, Stack, Table, Wordcloud, _default as default };
+export { Bar, BarHorizontal, IBarChartProps, IBarHorizontalChartProps, ILineChartProps, IPieChartProps, IScatterChartProps, IStackChartProps, ITableChartProps, IWordcloudChartProps, Line, Pie, Scatter, Stack, Table, Wordcloud, _default as default };
