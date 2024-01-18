def8ca2170fd7dd411077109d7b56332bf8f23ff
yuhongda <silverage.y@gmail.com>
2024-01-18 17:48:29 +0800

    chore: demo

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,11 +1,12 @@
+import { createRoot } from 'react-dom/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const root = createRoot(document.querySelector('#root')!)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
