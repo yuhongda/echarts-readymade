df9acd4861bd3e079284bc5cc42cc1d34a3814c4
yuhongda <silverage.y@gmail.com>
2026-07-30 21:47:29 +0800

    chore: vitest

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import path from 'node:path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/__test__/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    setupFiles: ['./__test__/setup.js'],
+    clearMocks: true,
+    testTimeout: 50000,
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './__test__/coverage',
+      reporter: ['json-summary', 'clover', 'json', 'lcov', 'text'],
+      include: ['packages/**/src/**/*.{js,jsx,ts,tsx}']
+    }
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, 'packages/wordcloud/src/assets')
+    }
+  }
+})
